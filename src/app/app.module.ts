f3d7987620343f17e6bc3f6f07d72cd2423733c7
Sid <siddharth@nisostech.com>
2019-01-24 00:12:20 +0530

    add angular materialize input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -18,9 +20,11 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [MatInputModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
